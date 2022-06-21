f5441dc1304daf4f19f7a6bb4ed72eab632b60f7
martin <barreiromartinj@gmail.com>
2022-06-21 16:08:08 -0300

    picture actualizacion

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,19 +1,22 @@
 import Tile from '../Tile/Tile';
 import './Main.scss';
 import traduction from '../../traducciones/traducciones'
+import mySelft from './mi-perfil.jpg'
 const Main = ({ lang }) => {
     const tiles = [
         {
             title: traduction[lang].title1,
             content: traduction[lang].contenido1,
             isMediaLeft: true,
-            media: "https://i.ibb.co/MZ1pKfM/emoji2.png"
+            media: mySelft,
+           
         },
         {
             title: traduction[lang].title2,
             content: traduction[lang].contenido2,
             isMediaLeft: false,
-            media: "https://tfagroup.co.uk/wp-content/uploads/2020/11/TFA-goals_blue-yellow.png"
+            media: "https://tfagroup.co.uk/wp-content/uploads/2020/11/TFA-goals_blue-yellow.png",
+           
         },
     ]
 
--- src/components/Tile/Tile.js
+++ src/components/Tile/Tile.js
@@ -1,21 +1,22 @@
 import './Tile.scss';
 
-const Tile = ({media, title, content, isMediaLeft}) => {
+const Tile = ({ media, title, content, isMediaLeft}) => {
 
     const mediaMarkup = () => (
         <div className="cmp-tile-media cmp-tile-wrapper">
-            <img src={media} alt={title} />
+            {isMediaLeft&& <img className='profile-picture' src={media} alt={title} />}
+            {!isMediaLeft&&<img  src={media} alt={title} />}
+            
         </div>
-        );
-
+    );
     return (
         <div className="cmp-tile">
-            { isMediaLeft &&  mediaMarkup() }
+            {isMediaLeft && mediaMarkup()}
             <div className="cmp-tile-content cmp-tile-wrapper">
                 <h4 className="cmp-tile-content__title">{title}</h4>
                 <div className="cmp-tile-content__text">{content}</div>
             </div>
-            { !isMediaLeft && mediaMarkup() }
+            {!isMediaLeft && mediaMarkup()}
         </div>
     )
 }
