936a0fb44ba3df5afdacd7311947b57da55132fa
Martin Barreiro <barreiromartinj@gmail.com>
2022-11-25 03:18:18 -0300

    add gitlab icon 3

--- src/components/Icons/Icons.js
+++ src/components/Icons/Icons.js
@@ -59,7 +59,9 @@ const Icons = ({ name }) => {
             {
                 name === 'gitlab' &&
                 <svg
-                    style={{ color: "black" }}
+                    style={{
+                        color: "black"
+                    }}
                     className={`cmp-icon cmp-icon-${name}`}
                     xmlns="http://www.w3.org/2000/svg"
                     width="55" height="55" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="1" stroke-linecap="round" stroke-linejoin="round" class="feather feather-gitlab">
